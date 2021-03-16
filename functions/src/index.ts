d799446cbdb01ddd1584170fd9f2fa66fd86bd27
Fidelis Yugita <fb46us@gmail.com>
2021-03-16 21:15:17 +0700

    minor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,9 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 import * as auto from "./auto";
+import * as place from "./place";
 
 if (!admin.apps.length) admin.initializeApp(functions.config().firebase);
 
 exports.auto = auto;
+exports.place = place;
